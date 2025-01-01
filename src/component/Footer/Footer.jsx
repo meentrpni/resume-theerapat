2a9dea8558d4696afd3332dcf6eca053437efb5e
meentrpni <meentrps@gmail.com>
2025-01-02 01:52:55 +0700

    edit footer3

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -6,16 +6,17 @@ import { BsTelephone } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
 function Footer() {
   return (
-        <footer className={styles.footer} >
+        <footer>
+          <div className={styles.footer_con}>
             <p>Copyright © 2025. All Rights Reserved.</p>
             <ul>
               <li><a href="tel:090-947-4165"><BsTelephone/><span>090-947-4165</span></a></li>
               <li><a href="mailto:meentrps@gmail.com"><CiMail/><span>meentrps@gmail.com</span></a></li>
               <li><a href="https://www.facebook.com/mtrpny" target="_blank"><FaFacebook/><span>Theerapat Nilyaem</span></a></li>
               <li><a href="https://line.me/ti/p/YpGNhTIees" target="_blank"><FaLine/><span>meentrp.</span></a></li>
-    
+
             </ul>
-    
+          </div>
         </footer>
   )
 }
